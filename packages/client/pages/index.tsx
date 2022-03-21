875e99567c3ca595d0af569a110d98dea9574030
itizawa <itizawa.pen@gmail.com>
2022-03-21 15:07:16 +0900

    🚑 test

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,12 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import { hello } from '../../shared/dist';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  hello();
-  hello('test!');
   return (
     <div className={styles.container}>
       <Head>

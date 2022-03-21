d09d7610f3bb9d5d453af515b425b1db1dfd43d7
itizawa <itizawa.pen@gmail.com>
2022-03-21 15:03:16 +0900

    🐛 test

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import { hello } from '@monorepo/shared/dist';
+import { hello } from '@monorepo/shared/src';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
